e000c01fcfd892da46ed9ecf2f1e7f54b88184da
DRaymond <danimator1@gmail.com>
2024-07-23 13:17:59 -0400

    updating backend

--- backend/playpulse_project/playpulse/admin.py
+++ backend/playpulse_project/playpulse/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Sport, Team, Player, Game, PlayerStats, TeamStats, Query, FunFact
 
-# Register your models here.
+admin.site.register(Sport)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Game)
+admin.site.register(PlayerStats)
+admin.site.register(TeamStats)
+admin.site.register(Query)
+admin.site.register(FunFact)
