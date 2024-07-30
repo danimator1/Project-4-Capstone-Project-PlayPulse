8fbeb494261c826619cb9d491ad51e9782515887
DRaymond <danimator1@gmail.com>
2024-07-30 14:32:11 -0400

    added updated css

--- frontend/playpulse/src/components/Nav.jsx
+++ frontend/playpulse/src/components/Nav.jsx
@@ -1,30 +1,29 @@
 import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './nav.css'
 
 export default function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div className="container">
-        <Link className="navbar-brand" to="/">PlayPulse</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/teamlist">Teams</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/playerlist">Players</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/playeroverviewlist">Players News</Link>
-            </li>
-          </ul>
-        </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top custom-navbar">
+      <Link className="navbar-brand" to="/">PlayPulse</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/teamlist">Teams</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/playerlist">Players</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/playeroverviewlist">Players News</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   )
